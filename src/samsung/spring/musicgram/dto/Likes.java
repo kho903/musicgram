c4f484026225723a19b7adaa7e85c2ab7fae7b71
­김혜인[컴퓨터공학부] <hyein15@kookmin.ac.kr>
2021-01-30 10:04:58 +0900

    좋아요 기능

---
+++ src/samsung/spring/musicgram/dto/Likes.java
@@ -0,0 +1,29 @@
+package samsung.spring.musicgram.dto;
+
+public class Likes {
+
+	private int like_no;
+	private int content_no;
+	private int user_id;
+	
+	public int getLike_no() {
+		return like_no;
+	}
+	public void setLike_no(int like_no) {
+		this.like_no = like_no;
+	}
+	public int getContent_no() {
+		return content_no;
+	}
+	public void setContent_no(int content_no) {
+		this.content_no = content_no;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	
+	
+}
--- src/samsung/spring/musicgram/service/ContentsService.java
+++ src/samsung/spring/musicgram/service/ContentsService.java
@@ -1,11 +1,16 @@
 package samsung.spring.musicgram.service;
 
+import java.util.HashMap;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import samsung.spring.musicgram.dao.ContentsMapper;
+import samsung.spring.musicgram.dao.LikesMapper;
 import samsung.spring.musicgram.dto.Contents;
 
 @Service
@@ -13,6 +18,9 @@ public class ContentsService {
 
 	@Autowired
 	private ContentsMapper contentsMapper;
+	
+	@Autowired
+	private LikesMapper likesMapper;
 
 	public Contents getContent(int content_no) {
 		return contentsMapper.getContent(content_no);
@@ -47,12 +55,37 @@ public class ContentsService {
 		return contentsMapper.getTagContents(tag);
 	}
 
-	public int pressLike(int content_no) {
-		return contentsMapper.pressLike(content_no);
+	public int isPressLike(int content_no, String user_id) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("content_no", content_no);
+		map.put("user_id", user_id);
+		return likesMapper.isPressLike(map);
+	}
+	public int pressLike(int content_no, String user_id) { 
+		//사용자가 좋아요를 눌렀을 때, 이전에 좋아요를 눌렀던 게시물인지 아닌지 확인하고
+		//좋아요를 안누른 게시물이였을 경우에 likes 테이블에 정보 추가, contents	테이블에 해당 content의 좋아요 갯수 올리기
+		//좋아요를 누른 게시물이였을때는 안함
+		//최종적으로 jsp에 좋아요 카운트 정보 뿌리기 위해서 getlike값 리턴.
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("content_no", content_no);
+		map.put("user_id", user_id);
+		if(likesMapper.isPressLike(map) == 0) { //좋아요 안누른 경우 
+			likesMapper.pressLike(map);
+			contentsMapper.pressLike(content_no);
+		}
+		return contentsMapper.getLike(content_no);
 	}
 
-	public int cancelLike(int content_no) {
-		return contentsMapper.cancelLike(content_no);
+	public int cancelLike(int content_no,  String user_id) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("content_no", content_no);
+		map.put("user_id", user_id);
+		if(likesMapper.isPressLike(map) == 1) { //좋아요를 눌렀을 경우 
+			likesMapper.cancelLike(map);
+			contentsMapper.cancelLike(content_no);
+		}
+		return contentsMapper.getLike(content_no);
 	}
 
 	public static String getYoutubeParse(String url) {
